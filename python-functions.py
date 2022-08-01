c613fda0f48fb46aaea5bd4cb881ba8431af6f05
JakeD <jakediefenderfer@gmail.com>
2022-08-01 19:57:16 -0400

    complete exercise 2

--- python-functions.py
+++ python-functions.py
@@ -1,7 +1,18 @@
-def sum_to(n):
-  sum = 0
-  for n in range(1, n + 1):
-    sum += n
-  return sum
+# def sum_to(n):
+#   sum = 0
+#   for n in range(1, n + 1):
+#     sum += n
+#   return sum
 
-print(sum_to(6))
+# print(sum_to(6))
+
+
+def largest(*nums):
+  largest = 0
+  for num in nums:
+    if num > largest:
+      largest = num
+  return largest
+
+
+print(largest(2, 4, 6, 10, 150))
